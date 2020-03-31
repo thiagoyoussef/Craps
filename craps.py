5878cfe3b768cb918e68aaa5d1b06cd1a2e47cfc
thiagoyoussef <thiagoyooussef@gmail.com>
2020-03-31 17:34:08 -0300

    Update craps.py

--- craps.py
+++ craps.py
@@ -1,2 +1,12 @@
+import random 
 fichas = 500
 tipo_aposta = input("Você irá fazer uma aposta: \nPass Line Bet\nField\nAny Craps\nTwelve\n")
+aposta = int(input("Quantas fichas você deseja apostar? "))
+soma_dados=random.randint(1,6)+random.randint(1,6)
+def twelve(aposta):
+    if soma_dados==12:
+        fichas+=aposta*30
+    else:
+        fichas=fichas-aposta
+
+           
