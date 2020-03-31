cf1f841c5bf391f2b052bebf0da496d022d3d151
agchieppe <agchieppe@users.noreply.github.com>
2020-03-31 17:57:39 -0300

    Update craps.py

--- craps.py
+++ craps.py
@@ -1,6 +1,6 @@
 import random 
 fichas=500
-tipo_aposta = input("Você irá fazer uma aposta: \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n")
+tipo_aposta = input("Você irá fazer uma aposta(retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n")
 aposta = int(input("Quantas fichas você deseja apostar? "))
 soma_dados=random.randint(1,6)+random.randint(1,6)
 def twelve(aposta,fichas,soma_dados):
