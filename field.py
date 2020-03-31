2b8b8dfedd8ad26214ec88839f6d4154bc725459
thiagoyoussef <thiagoyooussef@gmail.com>
2020-03-31 19:00:36 -0300

    Atualização funções

---
+++ field.py
@@ -0,0 +1,10 @@
+def field(aposta,fichas,soma_dados):
+    if soma_dados==5 or soma_dados==6 or soma_dados==7 or soma_dados==8:
+        fichas=fichas-aposta
+    elif soma_dados==3 or soma_dados==4 or soma_dados==9 or soma_dados==10 or soma_dados==11:
+        fichas+=aposta
+    elif soma_dados==2:
+        fichas+=aposta*2
+    elif soma_dados==12:
+        fichas+=aposta*3
+    
--- pass_line_bet.py
+++ pass_line_bet.py
@@ -4,8 +4,3 @@ def pass_line_bet(aposta,fichas,soma_dados):
     elif soma_dados==2 or soma_dados==3 or soma_dados==12:
         fichas=fichas-aposta
     elif soma_dados==4 or soma_dados==5 or soma_dados==6 or soma_dados==8 or soma_dados==9 or soma_dados==10:
-        fichas
-        
-
-
-            
