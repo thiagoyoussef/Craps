8a0505c888da2b2107fbfe56b3311aa731a6f80f
agchieppe <agchieppe@users.noreply.github.com>
2020-04-01 15:42:54 -0300

    Inicio point

--- craps.py
+++ craps.py
@@ -6,13 +6,23 @@ quer_sair = False
 estaPoint = False
 estaComeout = True
 
+#funcao anycraps
+def any_craps(aposta, soma_dados, fichas):
+    if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
+        fichas += (7*aposta)
+    else:
+        fichas -= aposta
+    return fichas
+
+
+
 while quer_sair == False or fichas != 0:
     sair = input('Gostaria de sair do jogo? ')
     if sair == 'sim' or sair == 'Sim':
         quer_sair = True
     else: #A partir desse else temos a fase comeout
         print('Voce esta na fase Come out')
-        tipo_aposta = int(input("Você irá fazer uma aposta(retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n"))
+        tipo_aposta = int(input("Qual aposta desejas fazer? (retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n"))
         aposta = int(input("Quantas fichas você deseja apostar? "))
         if soma_dados==7 or soma_dados==11:
             fichas+=aposta
@@ -20,6 +30,8 @@ while quer_sair == False or fichas != 0:
             fichas=fichas-aposta
         elif soma_dados==4 or soma_dados==5 or soma_dados==6 or soma_dados==8 or soma_dados==9 or soma_dados==10:
             estaComeout=False
-            estaPoint=True
-            tipo_aposta = int(input("Você irá fazer uma aposta(retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n"))
+            estaPoint = True
+        if estaPoint == True:
+            print('Voce esta na fase Point')
+            tipo_aposta = int(input("Qual aposta desejas fazer? (retorne com o número correspondente): Field(2)\nAny Craps(3)\nTwelve(4)\n"))
             aposta = int(input("Quantas fichas você deseja apostar? "))
