3a7c75889dcb930425e3f2d2f7db46f39bd0f356
thiagoyoussef <thiagoyooussef@gmail.com>
2020-04-01 15:58:07 -0300

    Come out

--- craps.py
+++ craps.py
@@ -22,21 +22,25 @@ while quer_sair == False or fichas != 0:
         quer_sair = True
     else: #A partir desse else temos a fase comeout
         print('Voce esta na fase Come out')
-        tipo_aposta = int(input("Qual aposta desejas fazer? (retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n"))
+        tipo_aposta=[]
+        append.tipo_aposta(int(input("Qual aposta desejas fazer? (retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n")))
         aposta = int(input("Quantas fichas você deseja apostar? "))
-        if tipo_aposta == 1:
+        aposta_adc=input("Você deseja realizar outro tipo de aposta? (s/n) ")
+        fichas-=aposta
+        if aposta_adc=="s":
+            append.tipo_aposta(int(input("Qual aposta desejas fazer? (retorne com o número correspondente): \nPass Line Bet(1)\nField(2)\nAny Craps(3)\nTwelve(4)\n"))))
+        else:
+            if tipo_aposta[0] == 1:
+                
 
-        
-
-
-        if soma_dados==7 or soma_dados==11:
-            fichas+=aposta
-        elif soma_dados==2 or soma_dados==3 or soma_dados==12:
-            fichas=fichas-aposta
-        elif soma_dados==4 or soma_dados==5 or soma_dados==6 or soma_dados==8 or soma_dados==9 or soma_dados==10:
-            estaComeout=False
-            estaPoint = True
-        if estaPoint == True:
-            print('Voce esta na fase Point')
-            tipo_aposta = int(input("Qual aposta desejas fazer? (retorne com o número correspondente): Field(2)\nAny Craps(3)\nTwelve(4)\n"))
-            aposta = int(input("Quantas fichas você deseja apostar? "))
+            if soma_dados==7 or soma_dados==11:
+                fichas+=aposta
+            elif soma_dados==2 or soma_dados==3 or soma_dados==12:
+                fichas=fichas-aposta
+            elif soma_dados==4 or soma_dados==5 or soma_dados==6 or soma_dados==8 or soma_dados==9 or soma_dados==10:
+                estaComeout=False
+                estaPoint = True
+            if estaPoint == True:
+                print('Voce esta na fase Point')
+                tipo_aposta = int(input("Qual aposta desejas fazer? (retorne com o número correspondente): Field(2)\nAny Craps(3)\nTwelve(4)\n"))
+                aposta = int(input("Quantas fichas você deseja apostar? "))
